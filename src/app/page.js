292eb5d85cba9c61c6087346dde1c2c75a8c0180
Louay Jrad <jrad.louay@outlook.com>
2024-02-16 04:58:37 +0100

    add home sections

--- src/app/page.js
+++ src/app/page.js
@@ -21,6 +21,72 @@ export default function Home() {
           <div className="relative w-[709px]"></div>
         </div>
       </section>
+      <section className="mt-12 mb-12">
+        <div className="container relative pt-[101px] pb-[101px] pl-[96.5px] pr-[96.5px]">
+          <Image
+            className="-z-10 rounded-lg"
+            src="/assets/home/desktop/image-speaker-zx7.jpg"
+            fill
+            objectFit="cover"
+            quality={100}
+            alt="product zx3"
+          ></Image>
+          <div className="pr-0.5 text-black flex-col justify-center items-start gap-8 inline-flex">
+            <h4>ZX7 SPEAKER</h4>
+            <button className="w-40 h-12 border border-black ">
+              See Product
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="mb-[200px]">
+        <div className="container grid grid-cols-2 gap-[30px] mb-[200px]">
+          <figure className="relative">
+            <Image
+              className="rounded-lg"
+              src={"/assets/home/desktop/image-earphones-yx1.jpg"}
+              fill
+              alt="image earphones-yx1"
+            />
+          </figure>
+          <div className="bg-zinc-100 rounded-lg pt-[101px] pb-[101px] pl-[96.5px] pr-[96.5px]">
+            <div className="text-black flex-col justify-center items-start gap-8 inline-flex">
+              <h4>ZX7 SPEAKER</h4>
+              <button className="w-40 h-12 border border-black ">
+                See Product
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mb-[200px]">
+        <div className="container grid grid-cols-2 gap-[30px]">
+          <div className="max-w-[445px] flex-col justify-center gap-8 flex">
+            <h2>
+              Bringing you the
+              <span className="text-orange-400"> best </span>
+              audio gear
+            </h2>
+            <div className="opacity-50 text-black body">
+              Located at the heart of New York City, Audiophile is the premier
+              store for high end headphones, earphones, speakers, and audio
+              accessories. We have a large showroom and luxury demonstration
+              rooms available for you to browse and experience a wide range of
+              our products. Stop by our store to meet some of the fantastic
+              people who make Audiophile the best place to buy your portable
+              audio equipment.
+            </div>
+          </div>
+          <div className="relative h-[588px]">
+            <Image
+              className="rounded-lg"
+              src="/assets/shared/desktop/image-best-gear.jpg"
+              fill
+              alt="best gear"
+            />
+          </div>
+        </div>
+      </section>
       {/* <section>
         <div className="container mt-[120px] mb-[168px] grid grid-cols-3 gap-x-[30px]">
           {Array(3)
